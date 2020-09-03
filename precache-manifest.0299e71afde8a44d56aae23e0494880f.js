d711b94a9f84b8ad8e708e15826c37d70094c11c
Jerry Dong <rtdong9421@yahoo.com>
2020-09-03 15:52:30 -0400

    Updates

---
+++ precache-manifest.0299e71afde8a44d56aae23e0494880f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5f9c3f93b0b715ac670bb40facbe402",
+    "url": "/oceanExplorer/index.html"
+  },
+  {
+    "revision": "0d4ef8d06a5495108c96",
+    "url": "/oceanExplorer/static/css/main.ad428a83.chunk.css"
+  },
+  {
+    "revision": "d21d08854ca3d39859b5",
+    "url": "/oceanExplorer/static/js/2.eb9ea471.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/oceanExplorer/static/js/2.eb9ea471.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d4ef8d06a5495108c96",
+    "url": "/oceanExplorer/static/js/main.e2112836.chunk.js"
+  },
+  {
+    "revision": "b075c284f69cfcd55a54",
+    "url": "/oceanExplorer/static/js/runtime-main.2dccc27e.js"
+  },
+  {
+    "revision": "b329823379c93e19725b70ca1d95d369",
+    "url": "/oceanExplorer/static/media/beach.b3298233.jpg"
+  },
+  {
+    "revision": "688a4e6a2f44e7ac21d5f25849fbe744",
+    "url": "/oceanExplorer/static/media/bottle.688a4e6a.png"
+  },
+  {
+    "revision": "11ae42b58aedca321d74203bf3e80dfe",
+    "url": "/oceanExplorer/static/media/cursor.11ae42b5.png"
+  },
+  {
+    "revision": "86f3517f875ad70002712c3477716148",
+    "url": "/oceanExplorer/static/media/paper.86f3517f.png"
+  },
+  {
+    "revision": "3f5c90e8d607da81b2597212829e41bb",
+    "url": "/oceanExplorer/static/media/waves.3f5c90e8.png"
+  }
+]);
