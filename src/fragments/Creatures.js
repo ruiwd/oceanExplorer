d63c03cd0aea5e002a29f7c8abc9d397e953a607
Jerry Dong <rtdong9421@yahoo.com>
2020-09-08 00:14:23 -0400

    add fish part two

--- src/fragments/Creatures.js
+++ src/fragments/Creatures.js
@@ -1,6 +1,15 @@
-import React, {Component, Fragment} from 'react';
+import React, {Component} from 'react';
 import spermWhale from '../assets/spermWhale.png';
 import squid from '../assets/squid.png';
+import blueWhale from '../assets/blueWhale.png';
+import lionfish from '../assets/lionfish.png';
+import dolphin from '../assets/dolphin.png';
+import tuna from '../assets/tuna.png';
+import sunfish from '../assets/sunfish.png';
+import ray from '../assets/ray.png';
+import shark from '../assets/shark.png';
+import anglerfish from '../assets/anglerfish.png';
+import eel from '../assets/eel.png';
 
 class Creatures extends Component {
   render () {
@@ -11,6 +20,33 @@ class Creatures extends Component {
         </div>
         <div className="creature squid">
           <img src={squid} alt="Colossal Squid"/>
+        </div>
+        <div className="creature blueWhale">
+          <img src={blueWhale} alt="Blue Whale"/>
+        </div>
+        <div className="creature lionfish">
+          <img src={lionfish} alt="Lionfish"/>
+        </div>
+        <div className="creature dolphin">
+          <img src={dolphin} alt="Bottlenose Dolphin"/>
+        </div>
+        <div className="creature tuna">
+          <img src={tuna} alt="Yellowfin Tuna"/>
+        </div>
+        <div className="creature sunfish">
+          <img src={sunfish} alt="Sunfish"/>
+        </div>
+        <div className="creature ray">
+          <img src={ray} alt="Manta Ray"/>
+        </div>
+        <div className="creature shark">
+          <img src={shark} alt="Frilled Shark"/>
+        </div>
+        <div className="creature anglerfish">
+          <img src={anglerfish} alt="Humpback Anglerfish"/>
+        </div>
+        <div className="creature eel">
+          <img src={eel} alt="Pelican Eel"/>
         </div>
       </div>
     )
