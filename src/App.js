61aa863023d21b6c6d763af67f30676019c30f86
Jerry Dong <rtdong9421@yahoo.com>
2020-09-15 13:30:49 -0400

    more attempts at fixing breakouts in touch-enabled devices

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, {Component, Fragment} from 'react';
-import Beach from './fragments/Beach';
-import OceanBG from './fragments/OceanBG';
-import './styles/styles.css';
+import React, { Component, Fragment } from "react";
+import Beach from "./fragments/Beach";
+import OceanBG from "./fragments/OceanBG";
+import "./styles/styles.css";
 
 class App extends Component {
   render() {
--- src/fragments/Cursor.js
+++ src/fragments/Cursor.js
@@ -1,26 +1,25 @@
-import React, {Component, Fragment} from 'react';
+import React, { Component, Fragment } from "react";
 
 class Cursor extends Component {
-    componentDidUpdate() {
-        const moveCursor = () => {
-            const cursor = document.getElementById('cursor');
-            let cursorY = this.props.cursorY;
-            let cursorX = this.props.cursorX;
+  componentDidUpdate() {
+    const moveCursor = () => {
+      const cursor = document.getElementById("cursor");
+      let cursorY = this.props.cursorY;
+      let cursorX = this.props.cursorX;
 
-            cursor.setAttribute('style', `top:${cursorY}px; left:${cursorX}px`)  
-        }
-    
-        moveCursor()
-    }
+      cursor.setAttribute("style", `top:${cursorY}px; left:${cursorX}px`);
+    };
 
-    render(){
-    return(
-        <Fragment>
-            <div id="cursor" className="cursor">
-            </div>
-        </Fragment>
-    )
-}
+    moveCursor();
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div id="cursor" className="cursor"></div>
+      </Fragment>
+    );
+  }
 }
 
 export default Cursor;
